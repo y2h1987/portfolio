c4547117a93f110b23cf13369d5aa005a4fd8364
Ryeo <Ryeoyh@gmail.com>
2020-02-02 13:47:11 +0800

    added new files

--- Python/SplitAndProcess(MultiThread).py
+++ Python/SplitAndProcess(MultiThread).py
@@ -10,7 +10,7 @@ from calendar import monthrange
 def processData(FileList):
 #===================================================================================
 
-    #this function is to read and save the process csv with the correct encoding into the relevant folders
+    #this function is to read and save the processed csv with the correct encoding into the relevant folders
     startTime_sub = datetime.now()
     Name = os.path.basename(FileList)
     print('LOAD : ' + Name + '  -  ' + str(startTime_sub))
@@ -23,6 +23,7 @@ def processData(FileList):
         print(head)
 
     #if there is a header , set header row else  set header as NONE
+    #finds the specified name in variable checkHeader
     if checkHeader.upper() in head.upper():
         headOffset = 0
     else:
@@ -46,22 +47,27 @@ def processData(FileList):
     print (df_Data.head(5))
     print('----SAVE : ' + Name + '  -  ' + str(datetime.now() - startTime_sub) + "\t RowsCount: " + str(len(df_Data)))
 
+    #Get Month Date from Dataframe
     dataDate = df_Data.loc[1,'CLOG_YYMM']
     dataDate = str(dataDate)
     year = int(dataDate[:4])
     mth = int(dataDate[5:6])
 
+    #Find Last day
     lastDay = monthrange(year,mth)[1]
     lastDay =  "{0:0=2d}".format(lastDay)
     mth = "{0:0=2d}".format(mth)
     fullDate = str(year) + str(mth) + str(lastDay)
 
+
+    #to set the number if file exist incrementally
     i = 1
     ext = '.txt'
-    fileName = 'KR_KR_MERITZ_SIPROD' + str(i) + '_' + fullDate + '_' + dataDate + ext
+    fileName = '<<FILENAME>>' + str(i) + '_' + fullDate + '_' + dataDate + ext
 
     print('Name: ' + fileName)
 
+    #check if the file number exist, if exist add a number behind
     while os.path.exists(procPath + fileName):
         i = i + 1
         fileName = 'KR_KR_MERITZ_SIPROD' + str(i) + '_' + fullDate + '_' + dataDate + ext
@@ -89,16 +95,18 @@ pd.options.display.float_format = '{:,.2f}'.format
 #########################
 ##### FILE SETTINGS #####
 #########################
-fpath = r'H:\01_Maps\KR_MERITZ\Data\To_Process\\'
+#PROGRAM WILL READ FROM THE FILEPATH FOLDER, IT WILL PROCESS AND SAVE INTO THE SAVEPATH, THE ORIGINAL FILE WILL BE MOVED TO ARCHIVE PATH
+fpath = r'<<FILEPATH>>'
 fname = '*.txt'
-procPath = r'H:\01_Maps\KR_MERITZ\Data\To_Load\\'
-archivePath =r'H:\01_Maps\KR_MERITZ\Data\To_Process\Archive\\'
+procPath = r'<<SAVEPATH>>'
+archivePath =r'<<ARCHIVEPATH>>'
 checkHeader = 'CLOG_YYMM'
-headerNames = ['CLOG_YYMM', 'POL_NO', 'SBCP_DT', 'UNT_PD_NM', 'PD_COV_NM', 'SBC_AMT', 'BDT_YMD', 'GNDR_CD', 'RSK_PREM', 'CSS_PREM', 'UNT_PD_CD', 'COV_CD', 'INS_PRD', 'OBJ_GRD_CD', 'INS_AGE']
+headerNames = ['CLOG_YYMM', 'POL_NO', 'SBCP_DT', ...]
 
 #########################
 ##### PROCESS FILES #####
 #########################
+#50 THREADS ARE OPEN TO RUN AS MANY AS 50 FILES AT THE SAME TIME
 with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
     for FileList in glob.glob(fpath + fname):
         executor.submit(processData, FileList)
